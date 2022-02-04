8e65af90a2b9694d96bc29e5cb75549dd8fc571f
Swisskill <wbrant@uwyo.edu>
2022-02-04 16:37:20 -0700

    major push. Everything functions. This iteration can be turned in

--- materials/lab/2/bhg-scanner/main/main.go
+++ materials/lab/2/bhg-scanner/main/main.go
@@ -8,10 +8,10 @@ import (
 func main() {
 	fmt.Println("Provide an address or type 'default' to use the default server")
 	var ad string
-	fmt.Scanln(ad)
+	fmt.Scanln(&ad)
 	if ad == "default" {
 		ad = "scanme.nmap.org:"
 	}
-	scanner.PortScanner(portMap.ProtoMap(), ad)
-https: //uwyo.zoom.us/j/3077664366?pwd=dWlVTXdtZjcxVEttOWZvT2t1M1hoZz09
+	scanner.PortScanner(ad)
+
 }
--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -7,8 +7,8 @@ import (
 // THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
 func TestOpenPort(t *testing.T) {
 
-	get, _ := PortScanner() // Currently function returns only number of open ports
-	want := 2               // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns
+	get, _ := PortScanner("scanme.nmap.org:") // Currently function returns only number of open ports
+	want := 2                                 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns
 	//consider what would happen if you parameterize the portscanner address and ports to scan
 
 	if get != want {
@@ -19,8 +19,8 @@ func TestOpenPort(t *testing.T) {
 func TestTotalPortsScanned(t *testing.T) {
 	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
 
-	get, got := PortScanner(protoMap map[int]string, ad string) // Currently function returns only number of open ports
-	want := 1024              // default value; consider what would happen if you parameterize the portscanner ports to scan
+	get, got := PortScanner("scanme.nmap.org:") // Currently function returns only number of open ports
+	want := 1024                                // default value; consider what would happen if you parameterize the portscanner ports to scan
 
 	if get+got != want {
 		t.Errorf("got %d, wanted %d", got, want)
