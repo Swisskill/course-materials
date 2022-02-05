f2d2580f7d7e399cad4375f140e0345c31e05311
Swisskill <wbrant@uwyo.edu>
2022-02-05 09:31:50 -0700

    Even bigger push. interface should be all working

--- materials/lab/2/bhg-scanner/main/main.go
+++ materials/lab/2/bhg-scanner/main/main.go
@@ -6,12 +6,53 @@ import (
 )
 
 func main() {
-	fmt.Println("Provide an address or type 'default' to use the default server")
 	var ad string
+	var range1, range2 int
+	fmt.Println("--------------------------------------------------------------")
+	fmt.Println("* * * * * * * * * * * Tread -- Carefully * * * * * * * * * * *")
+	fmt.Println("Provide an address or type 'default' to use the default server")
+	fmt.Println("--------------------------------------------------------------")
 	fmt.Scanln(&ad)
+	fmt.Println("")
 	if ad == "default" {
 		ad = "scanme.nmap.org:"
 	}
-	scanner.PortScanner(ad)
+
+	for i := 0; i < 1; i++ {
+		fmt.Println("--------------------------------------------------------------")
+		fmt.Println(" Specify the first digit of range or type 'default' to use 0  ")
+		fmt.Println("--------------------------------------------------------------")
+		fmt.Scanln(&range1)
+		fmt.Println("")
+		switch {
+		case range1 < 0:
+			fmt.Printf("The first digit can't be %d, it needs to be positive\n\n", range1)
+			i--
+		case range1 > 65506:
+			fmt.Printf("The first digit can't be %d, it needs to be less than 65506\n\n", range1)
+			i--
+		}
+	}
+
+	for i := 0; i < 1; i++ {
+		fmt.Println("--------------------------------------------------------------")
+		fmt.Println(" Specify the second digit of range or type 'default' to use 0  ")
+		fmt.Println("--------------------------------------------------------------")
+		fmt.Scanln(&range2)
+		fmt.Println("")
+		switch {
+		case range2 <= 0:
+			fmt.Printf("The second digit can't be %d, it needs to be positive and greater than 0\n\n", range2)
+			i--
+		case range2 > 65506:
+			fmt.Printf("The second digit can't be %d, it needs to be less than 65506\n\n", range2)
+			i--
+		case range2 < range1:
+			fmt.Printf("The second digit can't be %d, it needs to be bigger than %d\n\n", range2, range1)
+			i--
+		}
+	}
+	scanner.PortScanner(ad, range1, range2)
+	//scanner.PortScanner(ad)
 
 }
--- materials/lab/2/bhg-scanner/scanner/scanner_test.go
+++ materials/lab/2/bhg-scanner/scanner/scanner_test.go
@@ -1,14 +1,15 @@
 package scanner
 
 import (
+	"fmt"
 	"testing"
 )
 
 // THESE TESTS ARE LIKELY TO FAIL IF YOU DO NOT CHANGE HOW the worker connects (e.g., you should use DialTimeout)
 func TestOpenPort(t *testing.T) {
-
-	get, _ := PortScanner("scanme.nmap.org:") // Currently function returns only number of open ports
-	want := 2                                 // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns
+	fmt.Println("Alert: Tests use default condtions only. Edge cases must be tested in main")
+	get, _ := PortScanner("scanme.nmap.org:", 1, 1024) // Currently function returns only number of open ports
+	want := 2                                          // default value when passing in 1024 TO scanme; also only works because currently PortScanner only returns
 	//consider what would happen if you parameterize the portscanner address and ports to scan
 
 	if get != want {
@@ -18,9 +19,9 @@ func TestOpenPort(t *testing.T) {
 
 func TestTotalPortsScanned(t *testing.T) {
 	// THIS TEST WILL FAIL - YOU MUST MODIFY THE OUTPUT OF PortScanner()
-
-	get, got := PortScanner("scanme.nmap.org:") // Currently function returns only number of open ports
-	want := 1024                                // default value; consider what would happen if you parameterize the portscanner ports to scan
+	fmt.Println("Alert: Tests use default condtions only. Edge cases must be tested in main")
+	get, got := PortScanner("scanme.nmap.org:", 1, 1024) // Currently function returns only number of open ports
+	want := 1024                                         // default value; consider what would happen if you parameterize the portscanner ports to scan
 
 	if get+got != want {
 		t.Errorf("got %d, wanted %d", got, want)
