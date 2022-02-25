30c1d34432caa480f188e9fd02df34859b9dbc4a
Swisskill <wbrant@uwyo.edu>
2022-02-25 11:05:01 -0700

    Big push

--- materials/lab/3/main/main.go
+++ materials/lab/3/main/main.go
@@ -15,10 +15,40 @@ import (
 
 func main() {
 	os.Setenv("SHODAN_API_KEY", "gIiBqdKhe0Pl6yUkmplFnnp2HxK2befL")
-	if len(os.Args) != 2 {
-		fmt.Println(len(os.Args))
-		log.Fatalln("Usage: main <searchterm> | You need to have 2 arguments")
+	var ad int
+	for i := 0; i < 1; i++ {
+		fmt.Println("Please enter the number of filters you would like to use")
+		fmt.Scanln(&ad)
+		if ad < 1 || ad > 12 {
+			fmt.Println("The minimum is 1 filter and the maximum is 12 filters.")
+			i--
+		}
 	}
+	var mySlic []string
+	var slicer string
+	fmt.Println("\nPlease enter your facets one at a time")
+	for i := 0; i < ad; i++ {
+		fmt.Printf("Enter Facet number %d:\n", i+1)
+		fmt.Scanln(&slicer)
+		mySlic = append(mySlic, slicer)
+	}
+	var pag int
+	for i := 0; i < 1; i++ {
+		fmt.Println("\nPlease enter the number of pages you would like to view")
+		fmt.Scanln(&pag)
+		if ad < 1 {
+			fmt.Println("The minimum is page")
+			i--
+		}
+	}
+	//fmt.Print(mySlic)
+	/*
+		if len(os.Args) < 2 {
+			fmt.Println(len(os.Args))
+			log.Fatalln("Usage: main <searchterm> | You need to have at least 2 arguments (including main)")
+		}
+	*/
+
 	apiKey := os.Getenv("SHODAN_API_KEY")
 	s := shodan.New(apiKey)
 	info, err := s.APIInfo()
@@ -29,8 +59,15 @@ func main() {
 		"Query Credits: %d\nScan Credits:  %d\n\n",
 		info.QueryCredits,
 		info.ScanCredits)
+	/*
+		var argus []string //this should store all our args for narrow searching
+		for i := 1; i < len(os.Args); i++ {
+			argus = append(argus, os.Args[1])
+		}
+	*/
+	println(mySlic[0])
 
-	hostSearch, err := s.HostSearch(os.Args[1])
+	hostSearch, err := s.HostSearch(mySlic)
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -41,8 +78,9 @@ func main() {
 		h, _ := json.Marshal(host)
 		fmt.Println(string(h))
 		fmt.Println("==== end ", host.IPString, "====")
-		//fmt.Println("Press the Enter Key to continue.")
-		//fmt.Scanln()
+		fmt.Println("Press the Enter Key to continue.")
+		fmt.Scanln()
+
 	}
 
 	/*
