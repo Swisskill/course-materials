ba23228c3021e4e504111bda6ba3ebfc55c3ea81
Swisskill <wbrant@uwyo.edu>
2022-03-01 09:37:52 -0700

    pushing some first changes

--- materials/lab/4/main/main.go
+++ materials/lab/4/main/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 	"wyoassign/wyoassign"
-)
 
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	wyoassign.InitAssignments()
@@ -18,13 +19,26 @@ func main() {
 	router.HandleFunc("/api-status", wyoassign.APISTATUS).Methods("GET")
 	router.HandleFunc("/assignments", wyoassign.GetAssignments).Methods("GET")
 	router.HandleFunc("/assignment/{id}", wyoassign.GetAssignment).Methods("GET")
-	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")		
-	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")	
+	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")
+	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")
 	// router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
 
 	http.Handle("/", router)
 
 	//start and listen to requests
 	http.ListenAndServe(":8080", router)
+
+	//Now we begin the TUI
+
+	var pass string
+	var useR string
+	fmt.Println("(a little note, the password is hardcoded in goofy slice. Just look through the file. Pretend it's in another location)")
+	fmt.Println("Please enter your username to continue\n")
+	fmt.Scanln(&useR)
+	if useR == "drmike" {
+		fmt.Println("---Welcome Dr Mike---\nPlease enter your password")
+	} else {
+		fmt.Println("Please enter your password")
+	}
 
 }
---
+++ materials/lab/4/main/passW.go
@@ -0,0 +1,6 @@
+package main 
+var protoMap = map[string]string{
+"drmike" : "cedar",
+"wbrant" : "user"
+
+}
