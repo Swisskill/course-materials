f98e43d58046da7dfa40873969262c3e35270748
Swisskill <wbrant@uwyo.edu>
2022-03-11 09:49:02 -0700

    Updated the main for the updateAssignment

--- materials/lab/4/main/main.go
+++ materials/lab/4/main/main.go
@@ -20,6 +20,7 @@ func main() {
 	router.HandleFunc("/assignment/{id}", wyoassign.GetAssignment).Methods("GET")
 	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")
 	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")
+	router.HandleFunc("/assignment", wyoassign.UpdateAssignment).Methods("PUT")
 	// router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
 
 	http.Handle("/", router)
