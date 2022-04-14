40a2c0914954611c1e8685d5bef557d89d938979
Swisskill <wbrant@uwyo.edu>
2022-04-14 11:53:15 -0600

    test got and want didn't line up due to my error. fixed. could switch back to nickleback. what a dumb band name

--- materials/lab/7/hscan/hscan_test.go
+++ materials/lab/7/hscan/hscan_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestGuessSingle(t *testing.T) {
 	got := GuessSingle("77f62e3524cd583d698d51fa24fdff4f", "C:/Users/Will/Desktop/School/COSC/Cyber/course-materials/materials/lab/7/main/wordlist.txt") // Currently function returns only number of open ports
-	want := "Nickelback4life"
+	want := "password"
 	if got != want {
 		t.Errorf("got %s, wanted %s", got, want)
 	}
