0808b606582aee26b0ba00b0117664de9f9888ce
Swisskill <wbrant@uwyo.edu>
2022-04-18 14:32:02 -0600

    currently I have a broken test

--- materials/lab/7/hscan/hscan_test.go
+++ materials/lab/7/hscan/hscan_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestGuessSingle(t *testing.T) {
 	got := GuessSingle("77f62e3524cd583d698d51fa24fdff4f", "C:/Users/Will/Desktop/School/COSC/Cyber/course-materials/materials/lab/7/main/wordlist.txt") // Currently function returns only number of open ports
-	want := "password"
+	want := "Nickelback4life"
 	if got != want {
 		t.Errorf("got %s, wanted %s", got, want)
 	}
@@ -17,5 +17,24 @@ func TestGuessSingle(t *testing.T) {
 
 func TestGenHashMaps(t *testing.T) {
 	GenHashMaps("C:/Users/Will/Desktop/School/COSC/Cyber/course-materials/materials/lab/7/main/wordlist.txt")
+	got, _ := GetMD5("9701a1c165dd9420816bfec5edd6c2b1")
+	dontWant := ""
+	if got == dontWant {
+		t.Errorf("you failed the test for md5")
+	}
+
+}
+
+func TestSuite(t *testing.T) {
+	got := GuessSingle("77f62e3524cd583d698d51fa24fdff4f", "C:/Users/Will/Desktop/School/COSC/Cyber/course-materials/materials/lab/7/main/wordlist.txt") // Currently function returns only number of open ports
+	want := "Nickelback4life"
+	if got != want {
+		t.Errorf("got %s, wanted %s", got, want)
+	}
+	got2 := GuessSingle("77f62e3524cd583d698d51fa24fdff4f", "C:/Users/Will/Desktop/School/COSC/Cyber/course-materials/materials/lab/7/main/wordlist.txt") // Currently function returns only number of open ports
+	want2 := "Nickelback4life"
+	if got2 != want {
+		t.Errorf("got %s, wanted %s", got2, want2)
+	}
 
 }
